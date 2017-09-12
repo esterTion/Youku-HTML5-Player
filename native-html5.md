58232a7d1129b97c4f6cd8097b2d083a15bb0994
esterTion <lcz970@126.com>
2017-09-12 23:20:29 +0800

    1.3.0.3

--- native-html5.md
+++ native-html5.md
@@ -18,19 +18,12 @@ sessionStorage.removeItem('P_l_h5')
 - 没有播放速度改变界面（可手动在控制台改变）  
 - 整体界面处于初期阶段，没有动态效果、进度条预览等  
 - 部分视频cdn跨域配置不正确会导致无法播放，遇到console报错`Access-Control-Allow-Origin`相关请联系优酷，并带上cdn ip地址报错 
- 
----
-08/23  
-优酷已经停用对mac用户的html5播放器启用  
-![](http://wx2.sinaimg.cn/large/763783e4ly1fithf3k3m4j20h9092js7.jpg)  
 
 ---
-08/26  
+09/12  
 优酷目前启用html5的逻辑：  
 - http浏览，且  
-- 是webkit内核浏览器，且  
-- 不是Mac，且  
-- cna最后一位比D小[0-9A-C]  
-（不是很懂为什么例外mac用户） 
+- 不是IE  
+- 没有flash的浏览器，强制启用html5  
 
-![](http://wx1.sinaimg.cn/large/763783e4ly1fix2h76fetj20l805kwf9.jpg)
+![](http://wx4.sinaimg.cn/large/763783e4ly1fjh6qi5usqj20s00a03zx.jpg)
--- src/official_html5_enabler.js
+++ src/official_html5_enabler.js
@@ -16,13 +16,22 @@
         if (item.official_html5) {
             console.log('使用官方html5');
             if (location.href.match(/debug=h5/) == null) {
-                history.replaceState({}, '', '?debug=h5');
+                let replace = location.href.replace(/debug=[\w]+/, '');
+                replace += ((replace.indexOf('?') !== -1) ? '&' : '?') + 'debug=h5';
+                history.replaceState({}, '', replace);
             }
         } else if (!item.official_html5) {
             console.log('禁用官方html5');
             if (location.href.match(/debug=flv/) == null) {
-                history.replaceState({}, '', '?debug=flv');
+                let replace = location.href.replace(/debug=[\w]+/, '');
+                replace += ((replace.indexOf('?') !== -1) ? '&' : '?') + 'debug=flv';
+                history.replaceState({}, '', replace);
+            }
+            if (!navigator.plugins["Shockwave Flash"]) {
+                let script = document.createElement('script');
+                script.textContent = 'navigator.plugins["Shockwave Flash"]=true';
+                document.head.appendChild(script);
             }
         }
-    })
+    });
 })()
